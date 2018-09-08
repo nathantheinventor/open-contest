63699b219d80547c88c6c20219ea45384eb41849
Nathan Collins <nathantheinventor@gmail.com>
2018-09-08 11:19:57 -0400

    Installing the command the right way

--- publish.sh
+++ publish.sh
@@ -2,7 +2,8 @@
 if [ hash npm ]; then
     echo ""
 else
-    apt-get -y install node
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
 fi
 npm i typescript
 tsc
