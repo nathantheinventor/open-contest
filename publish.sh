884ac9a9e84cb1847a576ed5e6036cff20e1bcf9
Nathan Collins <nathantheinventor@gmail.com>
2018-09-08 11:20:47 -0400

    Trying again without sudo

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 if [ hash npm ]; then
     echo ""
 else
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+    curl -sL https://deb.nodesource.com/setup_10.x | bash
+    apt-get install -y nodejs
 fi
 npm i typescript
 tsc
