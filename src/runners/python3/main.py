9a1baf44197a6831641a2ce04579ee481d4d6912
Nathan Collins <nathantheinventor@gmail.com>
2018-09-25 19:33:10 -0400

    Created python3 runner

---
+++ src/runners/python3/main.py
@@ -0,0 +1,26 @@
+from threading import Timer, Thread
+import time
+import os
+import sys
+
+def kill():
+    print("TLE")
+    exit(1)
+
+testCases = int(sys.argv[1])
+timeLimit = int(sys.argv[2])
+
+def runCode(timeout):
+    for i in range(testCases):
+        if os.system("python3 /source/code.py < /source/in{0}.txt > /source/out{0}.txt 2> /source/err{0}.txt".format(i)) != 0:
+            print("runtime_error")
+            timeout.cancel()
+            exit(1)
+    print("ok")
+    timeout.cancel()
+    exit(0)
+
+timeout = Timer(timeLimit, kill)
+timeout.start()
+thread = Thread(target=runCode, args=(timeout,), daemon=True)
+thread.start()
