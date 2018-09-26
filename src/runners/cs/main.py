62cf770eda636e40708f6604b1943d66368d283c
Nathan Collins <nathantheinventor@gmail.com>
2018-09-25 20:26:50 -0400

    C# Runner

---
+++ src/runners/cs/main.py
@@ -0,0 +1,30 @@
+from threading import Timer, Thread
+import time
+import os
+import sys
+
+def kill():
+    print("TLE")
+    exit(1)
+
+testCases = int(sys.argv[1])
+timeLimit = int(sys.argv[2])
+
+def runCode(timeout):
+    if os.system("mcs /source/code.cs > /source/out/compile_out.txt 2> /source/out/compile_error.txt") != 0:
+        print("compile_error")
+        timeout.cancel()
+        exit(1)
+    for i in range(testCases):
+        if os.system("mono /source/code.exe < /source/in{0}.txt > /source/out/out{0}.txt 2> /source/out/err{0}.txt".format(i)) != 0:
+            print("runtime_error")
+            timeout.cancel()
+            exit(1)
+    print("ok")
+    timeout.cancel()
+    exit(0)
+
+timeout = Timer(timeLimit, kill)
+timeout.start()
+thread = Thread(target=runCode, args=(timeout,), daemon=True)
+thread.start()
