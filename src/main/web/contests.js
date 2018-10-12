ca6aed0a1af682714c39123fbb291e88a26d4c61
Nathan Collins <nathantheinventor@gmail.com>
2018-10-11 20:27:15 -0400

    Refactoring

---
+++ src/main/web/contests.js
@@ -0,0 +1,18 @@
+const util = require("../../util");
+const register = require("../../util/register");
+const db = require("../../util/db");
+
+register.post("/getContests", "admin", async _ => {
+    const contestIds = await util.db.listSubKeys("/contests");
+    let contests = [];
+    for (var id of contestIds) {
+        contests.push(await util.db.getKey(`/contests/${id}/contest.json`));
+    }
+    return contests;
+});
+
+register.post("/deleteContest", "admin", async (params) => {
+    const contestId = await params.id;
+    util.db.deleteKey(`/contests/${contestId}`);
+    return "ok";
+});
