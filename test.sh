4a3ae260444cf8dfcc52e867d40ed366af5dd16e
Nathan Collins <nathantheinventor@gmail.com>
2019-03-14 17:09:37 -0400

    Making directories relative

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ~/db:/db \
     -v /tmp:/tmp \
-    -v /Users/nathancollins/code/open-contest/src/main/:/code \
-    -v /Users/nathancollins/code/open-contest/src/main/nginx.conf:/etc/nginx/sites-enabled/nginx.conf \
+    -v ~/code/open-contest/src/main/:/code \
+    -v ~/code/open-contest/src/main/nginx.conf:/etc/nginx/sites-enabled/nginx.conf \
     -p 0.0.0.0:8000:8000/tcp \
     $USER/$OC_PROJECT_NAME
